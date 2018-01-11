7c43f1b431cd2f6f78db381e74becfaa2330ec67
Joshua Fredrickson <uafredrickson@gmail.com>
2018-01-10 23:14:44 -0800

    adding functionality

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function addTask() {
+    var $btn = $('.add');
+    var $input = $('section input');
+    var $list = $('.task ul');
+
+    $btn.on('click', function() {
+        var val = $input.val();
+        $input.val('');
+        $list.append('<li>' + val + '</li>');
+    });
+}
+
+
+function imgSwapper() {
+    $('select').on('change', function() {
+        var $imgSrc = $(this.val();
+        $('.main-img').attr('src', $imgSrc);
+    )
+    })
+}
+
